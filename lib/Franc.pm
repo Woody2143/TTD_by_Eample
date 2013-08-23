6d3fae6b741217e26d4e100f06d656e96a7e4c92
Brandon Wood <woody@2143.net>
2013-08-22 22:11:21 -0400

    moved equals method out of Franc since it is in Money

--- lib/Franc.pm
+++ lib/Franc.pm
@@ -3,19 +3,10 @@ use Moose;
 use MooseX::Method::Signatures;
 use MooseX::Privacy;
 
-has 'amount' => (
-       isa => 'Int',
-        is => 'ro',
-    traits => [qw/Private/],
-);
+extends 'Money';
 
 method times (Int $multiplier) {
     return Franc->new( amount => ($self->amount * ${multiplier}));
-}
-
-method equals (Franc $object) {
-    return 1 if $self->amount == ${object}->amount;
-    return 0;
 }
 
 no Moose;
